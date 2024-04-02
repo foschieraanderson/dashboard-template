40c82e8db1abc65acfac7eaa0d2e01dfd29c01c9
Anderson Foschiera <foschieraanderson@gmail.com>
2024-04-02 03:09:47 -0300

    feat: Add start front with vuejs and tailwind

---
+++ app/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-zinc-900 flex justify-center items-center">
+      <h1 class="text-white">Hello World</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
