fbb1e8e3279240c3047a3ea561ef95c38722dcc8
Anderson Foschiera <foschieraanderson@gmail.com>
2024-07-10 14:30:23 -0300

    feat: Add user route to create and get user by ID

--- api/src/routes/user_routes.py
+++ api/src/routes/user_routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends
+from typing import List
+from pydantic import UUID4
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.configs.database import db_session
 
@@ -7,14 +9,20 @@ from src.services.user_service import UserService
 
 router = APIRouter()
 
-@router.get("/")
-async def users() -> str:
-    return "tudo certo!"
+@router.get("", status_code=status.HTTP_200_OK, response_model=List[UserResponse])
+async def get_users(session: AsyncSession = Depends(db_session)) -> List[UserResponse]:
+    _service = UserService(session)
+    return await _service.get_all()
 
-@router.post("", status_code=201, response_model=UserResponse)
+@router.get("/{_id}", status_code=status.HTTP_200_OK, response_model=UserResponse)
+async def get_user_by_id(_id: UUID4, session: AsyncSession = Depends(db_session)) -> UserResponse:
+    _service = UserService(session)
+    return await _service.get_by_id(_id)
+
+@router.post("", status_code=status.HTTP_201_CREATED, response_model=UserResponse)
 async def create_user(
         data: UserSchema,
         session: AsyncSession = Depends(db_session),
-):
+) -> UserResponse:
     _service = UserService(session)
     return await _service.create(data)
