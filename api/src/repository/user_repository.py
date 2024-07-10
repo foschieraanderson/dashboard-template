776d827ec7aca6bc8805ce5f36d450d06d783d2e
Anderson Foschiera <foschieraanderson@gmail.com>
2024-07-10 14:28:54 -0300

    feat: Add user repository

--- api/src/repository/user_repository.py
+++ api/src/repository/user_repository.py
@@ -1,3 +1,6 @@
+from typing import List, Optional, Type
+from pydantic import UUID4
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.models.user_model import User
 
@@ -16,4 +19,14 @@ class UserRepository:
         await self.session.commit()
         await self.session.refresh(user)
         return UserResponse(**user.__dict__)
+
+    async def get_all(self) -> List[Optional[UserResponse]]:
+        """Get all users"""
+        users = await self.session.execute(select(User))
+        return users.scalars()
+
+    async def get_by_id(self, _id: UUID4) -> UserResponse:
+        """Get user by ID"""
+        user = await self.session.execute(select(User).where(User.uuid==_id))
+        return user.scalar()
 
